7aeb56ea3d8b397fe011b29bced8bf9afd3c1e20
irisfaraway <iris.faraway@gmail.com>
2022-03-21 14:31:51 +0000

    Make the change I meant to make

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 if (time < 10) {
   greeting = "Good morning!!";
 } else if (time < 20) {
-  greeting = "Good day to you, friend! Hope you are well";
+  greeting = "Good day to you, friend! Hope you are very well";
 } else {
   greeting = "Good evening!!";
 }
