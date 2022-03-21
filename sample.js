27452c9f8559648f7407889f4393229ba6bf59a0
irisfaraway <iris.faraway@gmail.com>
2022-03-21 14:30:17 +0000

    Update message

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 if (time < 10) {
   greeting = "Good morning!!";
 } else if (time < 20) {
-  greeting = "Good day!!";
+  greeting = "Good day to you, friend! Hope you are well";
 } else {
   greeting = "Good evening!!";
 }
